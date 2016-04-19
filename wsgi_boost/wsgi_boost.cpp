23d0b6960472508a4ab22b604cdca6b3463b3c70
Roman Miroshnychenko <romanvm@yandex.ua>
2016-04-19 14:35:57 +0300

    Adds Boost Python module wrapper

--- wsgi_boost/wsgi_boost.cpp
+++ wsgi_boost/wsgi_boost.cpp
@@ -4,3 +4,16 @@ Boost Python wrapper for the core WSGI/HTTP servers
 Copyright (c) 2016 Roman Miroshnychenko <romanvm@yandex.ua>
 License: MIT, see License.txt
 */
+
+#include "server_http.h"
+
+using namespace boost::python;
+
+
+BOOST_PYTHON_MODULE(wsgi_boost)
+{
+	PyEval_InitThreads(); // Initialize GIL
+
+	scope current;
+	current.attr("__doc__") = "This module provides WSGI/HTTP server class";
+}
