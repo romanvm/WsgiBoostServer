51e73f65fb58aa498b3e991cb29d65b9ea450472
Roman Miroshnychenko <romanvm@yandex.ua>
2016-04-19 13:16:15 +0300

    Adds BaseRequestHandler class

--- wsgi_boost/request_handlers.h
+++ wsgi_boost/request_handlers.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+
+
+class BaseRequestHandler
+{
+private:
+	std::ostream& _response;
+	const std::string& _server_name;
+	const std::string& _http_version;
+
+public:
+	BaseRequestHandler(std::ostream& response, const std::string& server_name, const std::string& http_version):
+		_response{response}, _server_name{server_name}, _http_version{http_version} {}
+	virtual ~BaseRequestHandler() {}
+
+	virtual void send_code(const std::string& code, const std::string message = "")
+	{
+		_response << "HTTP/" << _http_version << " " << code << "\r\n";
+		_response << "Server: " << _server_name << "\r\n";
+		_response << "Content-Length: " << message.length() << "\r\n";
+		_response << "Content-Type: text/plain\r\nConnection: close\r\n\r\n" << message;
+	}
+};
