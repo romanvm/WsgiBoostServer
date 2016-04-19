a09149f7fda6b6193a5617cb7b3e8a3083a49d29
Roman Miroshnychenko <romanvm@yandex.ua>
2016-04-19 14:23:03 +0300

    Adds core http server engine, adds license, adds WsgiBoost namespace

--- wsgi_boost/request_handlers.h
+++ wsgi_boost/request_handlers.h
@@ -1,28 +1,36 @@
 #pragma once
+/*
+Request handlers for WSGI apps and static files
+
+Copyright (c) 2016 Roman Miroshnychenko <romanvm@yandex.ua>
+License: MIT, see License.txt
+*/
 
 #include "utils.h"
 
 #include <iostream>
 
-
-class BaseRequestHandler
+namespace WsgiBoost
 {
-protected:
-	std::ostream& _response;
-	const std::string& _server_name;
-	const std::string& _http_version;
-
-public:
-	BaseRequestHandler(std::ostream& response, const std::string& server_name, const std::string& http_version):
-		_response{response}, _server_name{server_name}, _http_version{http_version} {}
-	virtual ~BaseRequestHandler() {}
-
-	virtual void send_code(const std::string& code, const std::string message = "")
+	class BaseRequestHandler
 	{
-		_response << "HTTP/" << _http_version << " " << code << "\r\n";
-		_response << "Server: " << _server_name << "\r\n";
-		_response << "Date: " << get_current_gmt_time() << "\r\n";
-		_response << "Content-Length: " << message.length() << "\r\n";
-		_response << "Content-Type: text/plain\r\nConnection: close\r\n\r\n" << message;
-	}
-};
+	protected:
+		std::ostream& _response;
+		const std::string& _server_name;
+		const std::string& _http_version;
+
+	public:
+		BaseRequestHandler(std::ostream& response, const std::string& server_name, const std::string& http_version) :
+			_response{ response }, _server_name{ server_name }, _http_version{ http_version } {}
+		virtual ~BaseRequestHandler() {}
+
+		virtual void send_code(const std::string& code, const std::string message = "")
+		{
+			_response << "HTTP/" << _http_version << " " << code << "\r\n";
+			_response << "Server: " << _server_name << "\r\n";
+			_response << "Date: " << get_current_gmt_time() << "\r\n";
+			_response << "Content-Length: " << message.length() << "\r\n";
+			_response << "Content-Type: text/plain\r\nConnection: close\r\n\r\n" << message;
+		}
+	};
+}
---
+++ wsgi_boost/wsgi_boost.cpp
@@ -0,0 +1,6 @@
+/*
+Boost Python wrapper for the core WSGI/HTTP servers
+
+Copyright (c) 2016 Roman Miroshnychenko <romanvm@yandex.ua>
+License: MIT, see License.txt
+*/
