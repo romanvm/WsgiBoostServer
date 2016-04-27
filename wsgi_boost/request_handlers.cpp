f012f9124ab5318b08b65a42fe58c2a43f27984c
Roman Miroshnychenko <romanvm@yandex.ru>
2016-04-27 23:31:18 +0300

    Removes changes from previous commit

--- wsgi_boost/request_handlers.cpp
+++ wsgi_boost/request_handlers.cpp
@@ -171,7 +171,7 @@ namespace wsgi_boost
 	WsgiRequestHandler::WsgiRequestHandler(ostream& response, std::shared_ptr<Request> request, py::object& app) :
 		BaseRequestHandler(response, request), m_app{ app }
 	{
-		std::function<py::object(py::str, py::list, py::object)> sr{
+		std::function<void(py::str, py::list, py::object)> sr{
 			[this](py::str status, py::list headers, py::object exc_info = py::object())
 			{
 				if (!exc_info.is_none())
@@ -186,12 +186,11 @@ namespace wsgi_boost
 					py::object header = headers[i];
 					this->out_headers.emplace_back(py::extract<char*>(header[0]), py::extract<char*>(header[1]));
 				}
-				return py::import("io").attr("BytesIO");
 			}
 		};
 		m_start_response = py::make_function(sr, py::default_call_policies(),
 			(py::arg("status"), "headers", py::arg("exc_info") = py::object()),
-			boost::mpl::vector<py::object, py::str, py::list, py::object>());
+			boost::mpl::vector<void, py::str, py::list, py::object>());
 	}
 
 
