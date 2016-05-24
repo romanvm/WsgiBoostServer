d8e64a6644c6126604d3d76f6b4bbc6775b4ca7e
Roman Miroshnychenko <romanvm@yandex.ua>
2016-05-24 20:55:11 +0300

    Tests: WIP

--- test_wsgi_boost/tests.py
+++ test_wsgi_boost/tests.py
@@ -8,6 +8,7 @@ import threading
 import time
 import unittest
 import requests
+from wsgiref.validate import validator
 
 print('Running Python tests')
 
@@ -16,6 +17,21 @@ os.chdir(cwd)
 sys.path.insert(0, os.path.dirname(cwd))
 
 import wsgi_boost
+
+
+init_old = threading.Thread.__init__
+def init(self, *args, **kwargs):
+    init_old(self, *args, **kwargs)
+    run_old = self.run
+    def run_with_except_hook(*args, **kw):
+        try:
+            run_old(*args, **kw)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            sys.excepthook(*sys.exc_info())
+    self.run = run_with_except_hook
+threading.Thread.__init__ = init
 
 
 class ServingStaticFilesTestCase(unittest.TestCase):
@@ -63,6 +79,51 @@ class ServingStaticFilesTestCase(unittest.TestCase):
         self.assertEqual(resp.status_code, 200)
         resp = requests.get('http://127.0.0.1:8000/static/index.html', headers={'If-Modified-Since': 'Mon, 22 Dec 2014 15:00:00 GMT'})
         self.assertEqual(resp.status_code, 304)
+
+
+class App(object):
+    """
+    A test WSGI application
+    """
+    def __call__(self, environ, start_response):
+        self.environ = environ
+        self.start_response = start_response
+        content == 'App OK'
+        if self.environ['PATH_INFO'] == '/test_write':
+            content = 'Write OK'
+        write = start_response('200 OK', [('Content-type', 'text/plain'), ('Content-Length', str(len(content)))])
+        if content == 'Write OK':
+            write(content)
+            content = ''
+        return [content]
+
+
+class WsgiServerTestCase(unittest.TestCase):
+    def setUp(self):
+        self._httpd = wsgi_boost.WsgiBoostHttp(num_threads=1)
+        self._httpd.wsgi_debug = True
+        app = App()
+        self._httpd.set_app(validator(app))
+        self._server_thread = threading.Thread(target=cls._httpd.start)
+        self._server_thread.start()
+
+    def tearDown(self):
+        self._server_thread.join()
+        del cls._httpd
+
+    def test_wsgi_app_response(self):
+        resp = requests.get('http://127.0.0.1:8000/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('App OK' in resp.text)
+        self._httpd.stop()
+        time.sleep(0.1)
+
+    def test_write_function(self):
+        resp = requests.get('http://127.0.0.1:8000/test_write')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('Write OK.' in resp.text)
+        self._httpd.stop()
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
