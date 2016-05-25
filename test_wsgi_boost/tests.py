609b38400da34894fe18a2c78ed8bcbc35bece21
Roman Miroshnychenko <romanvm@yandex.ua>
2016-05-25 15:35:28 +0300

    Finishes tests for core functionality

--- test_wsgi_boost/tests.py
+++ test_wsgi_boost/tests.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""
+Test suite for testing core WsgiBoostServer functionality
+"""
 
 from __future__ import print_function
 import os
@@ -7,65 +10,14 @@ import sys
 import threading
 import time
 import unittest
-import requests
 from wsgiref.validate import validator
-
-print('Running Python tests')
+import requests
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 os.chdir(cwd)
 sys.path.insert(0, os.path.dirname(cwd))
 
 import wsgi_boost
-
-
-class ServingStaticFilesTestCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls._httpd = wsgi_boost.WsgiBoostHttp(num_threads=1)
-        cls._httpd.add_static_route('^/static', cwd)
-        cls._httpd.add_static_route('^/invalid_dir', '/foo/bar/baz/')
-        cls._server_thread = threading.Thread(target=cls._httpd.start)
-        cls._server_thread.daemon = True
-        cls._server_thread.start()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls._httpd.stop()
-        cls._server_thread.join()
-        del cls._httpd
-
-    def test_forbidden_http_methods(self):
-        resp = requests.post('http://127.0.0.1:8000/static')
-        self.assertEqual(resp.status_code, 405)
-
-    def test_invalid_content_directory(self):
-        resp = requests.get('http://127.0.0.1:8000/invalid_dir')
-        self.assertEqual(resp.status_code, 500)
-        self.assertTrue('Invalid content directory' in resp.text)
-
-    def test_accessing_static_file(self):
-        resp = requests.get('http://127.0.0.1:8000/static/index.html')
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue('Welcome to Our Company' in resp.text)
-        resp = requests.get('http://127.0.0.1:8000/static/foo/bar.html')
-        self.assertEqual(resp.status_code, 404)
-
-    def test_gzipping_static_files(self):
-        html_size = os.path.getsize('index.html')
-        resp = requests.get('http://127.0.0.1:8000/static/index.html')
-        self.assertTrue(html_size > int(resp.headers['Content-Length']))
-        png_size = os.path.getsize('profile_pic.png')
-        resp = requests.get('http://127.0.0.1:8000/static/profile_pic.png')
-        self.assertEqual(png_size, int(resp.headers['Content-Length']))
-
-    def test_if_modified_since(self):
-        last_modified = time.mktime(time.strptime('2014-12-21 17:20:00', '%Y-%m-%d %H:%M:%S'))
-        os.utime(os.path.join(cwd, 'index.html'), (last_modified, last_modified))
-        resp = requests.get('http://127.0.0.1:8000/static/index.html', headers={'If-Modified-Since': 'Sat, 20 Dec 2014 15:00:00 GMT'})
-        self.assertEqual(resp.status_code, 200)
-        resp = requests.get('http://127.0.0.1:8000/static/index.html', headers={'If-Modified-Since': 'Mon, 22 Dec 2014 15:00:00 GMT'})
-        self.assertEqual(resp.status_code, 304)
 
 
 class App(object):
@@ -86,6 +38,10 @@ class App(object):
             content = self.test_input_read_limited()
         elif self.environ['PATH_INFO'] == '/test_input_readline':
             content = self.test_input_readline()
+        elif self.environ['PATH_INFO'] == '/test_input_readlines':
+            content = self.test_input_readlines()
+        elif self.environ['PATH_INFO'] == '/test_input_iterator':
+            content = self.test_input_iterator()
         elif self.environ['PATH_INFO'] == '/test_write':
             content = 'Write OK'
         write = start_response('200 OK', [('Content-type', 'text/plain'), ('Content-Length', str(len(content)))])
@@ -116,6 +72,21 @@ class App(object):
         line = self.environ['wsgi.input'].readline()
         assert line == 'Mark Twain. The Awful German Language\n'
         return 'Input readline OK'
+
+    def test_input_readlines(self):
+        lines = self.environ['wsgi.input'].readlines()
+        assert isinstance(lines, list)
+        assert len(lines) == 61
+        content = ''.join(lines)
+        assert len(content) == 4194
+        return 'Input readlines OK'
+
+    def test_input_iterator(self):
+        lines = []
+        for line in self.environ['wsgi.input']:
+            lines.append(line)
+        assert len(lines) == 61
+        return 'Input iterator OK'
 
 
 class ValidateWsgiServerTestCase(unittest.TestCase):
@@ -181,6 +152,65 @@ class WsgiServerFunctionsTestCase(unittest.TestCase):
         resp = requests.post('http://127.0.0.1:8000/test_input_readline', data=self._data)
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('Input readline OK' in resp.text)
+
+    def test_input_readlines(self):
+        resp = requests.post('http://127.0.0.1:8000/test_input_readlines', data=self._data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('Input readlines OK' in resp.text)
+
+    def test_input_iterator(self):
+        resp = requests.post('http://127.0.0.1:8000/test_input_iterator', data=self._data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('Input iterator OK' in resp.text)
+
+
+class ServingStaticFilesTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._httpd = wsgi_boost.WsgiBoostHttp(num_threads=1)
+        cls._httpd.add_static_route('^/static', cwd)
+        cls._httpd.add_static_route('^/invalid_dir', '/foo/bar/baz/')
+        cls._server_thread = threading.Thread(target=cls._httpd.start)
+        cls._server_thread.daemon = True
+        cls._server_thread.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._httpd.stop()
+        cls._server_thread.join()
+        del cls._httpd
+
+    def test_forbidden_http_methods(self):
+        resp = requests.post('http://127.0.0.1:8000/static')
+        self.assertEqual(resp.status_code, 405)
+
+    def test_invalid_content_directory(self):
+        resp = requests.get('http://127.0.0.1:8000/invalid_dir')
+        self.assertEqual(resp.status_code, 500)
+        self.assertTrue('Invalid content directory' in resp.text)
+
+    def test_accessing_static_file(self):
+        resp = requests.get('http://127.0.0.1:8000/static/index.html')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('Welcome to Our Company' in resp.text)
+        resp = requests.get('http://127.0.0.1:8000/static/foo/bar.html')
+        self.assertEqual(resp.status_code, 404)
+
+    def test_gzipping_static_files(self):
+        html_size = os.path.getsize('index.html')
+        resp = requests.get('http://127.0.0.1:8000/static/index.html')
+        self.assertTrue(html_size > int(resp.headers['Content-Length']))
+        png_size = os.path.getsize('profile_pic.png')
+        resp = requests.get('http://127.0.0.1:8000/static/profile_pic.png')
+        self.assertEqual(png_size, int(resp.headers['Content-Length']))
+
+    def test_if_modified_since(self):
+        last_modified = time.mktime(time.strptime('2014-12-21 17:20:00', '%Y-%m-%d %H:%M:%S'))
+        os.utime(os.path.join(cwd, 'index.html'), (last_modified, last_modified))
+        resp = requests.get('http://127.0.0.1:8000/static/index.html', headers={'If-Modified-Since': 'Sat, 20 Dec 2014 15:00:00 GMT'})
+        self.assertEqual(resp.status_code, 200)
+        resp = requests.get('http://127.0.0.1:8000/static/index.html', headers={'If-Modified-Since': 'Mon, 22 Dec 2014 15:00:00 GMT'})
+        self.assertEqual(resp.status_code, 304)
 
 
 if __name__ == '__main__':
