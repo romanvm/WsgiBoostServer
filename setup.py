c6b71194992b0a27a4479e5d6a19ec23fae5ddf9
Roman Miroshnychenko <romanvm@yandex.ru>
2016-05-28 22:06:04 +0300

    Refactors setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python
 
+import os
 import sys
 from distutils.core import setup
 from distutils.extension import Extension
 
 
-if sys.platform == 'win32' :
-    include_dirs = ['d:/boost_1_60_0', './wsgi_boost']
+class BuildError(Exception):
+    pass
+
+extra_compile_args = ['-std=c++11']
+
+if sys.platform == 'win32':
+    try:
+        boost_root = os.environ['BOOST_ROOT']
+    except KeyError:
+        raise BuildError('%BOOST_ROOT% variable is not defined!')
+    try:
+        boost_librarydir = os.environ['BOOST_LIBRARYDIR']
+    except KeyError:
+        raise BuildError('%BOOST_LIBRARYDIR% is not defined!')
+    include_dirs = ['./wsgi_boost', boost_root]
     libraries=[
-        'boost_regex',
-        'boost_system',
-        'boost_thread',
-        'boost_coroutine',
-        'boost_context',
-        'boost_filesystem',
-        'boost_date_time',
-        'boost_iostreams',
-        'boost_python',
-        'boost_zlib',
+        'mswsock',
+        'ws2_32',
+        'boost_regex-mt-s',
+        'boost_system-mt-s',
+        'boost_thread-mt-s',
+        'boost_coroutine-mt-s',
+        'boost_context-mt-s',
+        'boost_filesystem-mt-s',
+        'boost_date_time-mt-s',
+        'boost_iostreams-mt-s',
+        'boost_python-mt-s',
+        'boost_zlib-mt-s',
         ]
-    library_dirs=['d:/boost_1_60_0/']
-else :
-    include_dirs = ['/usr/include/boost', './wsgi_boost']
+    library_dirs=[boost_librarydir]
+    extra_compile_args.append('-DBOOST_PYTHON_STATIC_LIB')
+else:
+    include_dirs = ['./wsgi_boost']
     libraries=[
         'boost_regex',
         'boost_system',
@@ -34,7 +51,7 @@ else :
         'boost_python',
         'z',
         ]
-    library_dirs=['/usr/lib/i386-linux-gnu']
+    library_dirs=[]
 
 files = [
     './wsgi_boost/connection.cpp',
@@ -45,13 +62,14 @@ files = [
     './wsgi_boost/wsgi_boost.cpp',
     ]
 
-setup(name='wsgi_boost',    
+setup(name='wsgi_boost',
       ext_modules=[
-                    Extension('wsgi_boost',
-                              files,
-                              library_dirs=library_dirs,
-                              libraries=libraries,
-                              include_dirs=include_dirs,
-                              depends=[]),
-                    ]
-     )
+          Extension('wsgi_boost',
+                    files,
+                    library_dirs=library_dirs,
+                    libraries=libraries,
+                    include_dirs=include_dirs,
+                    extra_compile_args=extra_compile_args,
+                    depends=[])
+          ]
+      )
