7e0fa00252fd73c9c47f1d3155fb1842919e934a
Roman Miroshnychenko <romanvm@yandex.ua>
2016-05-30 12:44:21 +0300

    Refactors setup.py to make it more generic on Windows

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from distutils.msvc9compiler import get_build_version
 try:
     from setuptools import setup
     from setuptools.extension import Extension
@@ -22,18 +23,29 @@ files = [
     './wsgi_boost/server.cpp',
     './wsgi_boost/wsgi_boost.cpp',
     ]
-
 include_dirs = ['./wsgi_boost']
+libraries = [
+    'boost_regex',
+    'boost_system',
+    'boost_thread',
+    'boost_coroutine',
+    'boost_context',
+    'boost_filesystem',
+    'boost_date_time',
+    'boost_iostreams',
+    'boost_python',
+    ]
 define_macros = [('BOOST_ALL_NO_LIB', None)]
 library_dirs=[]
 extra_compile_args = []
 extra_link_args = []
-
 if sys.platform == 'win32':
+    # Monkey-patching the default distutils compliler to MSVS 2015
+    vs_comntools = 'VS{0}COMNTOOLS'.format(int(get_build_version() * 10))
     try:
-        os.environ['VS90COMNTOOLS'] = os.environ['VS140COMNTOOLS']
+        os.environ[vs_comntools] = os.environ['VS140COMNTOOLS']
     except KeyError:
-        raise BuildError('Compatible compiler not found! Only MSVS 2015 SP2+ is supported.')
+        raise BuildError('Compatible compiler not found! Only MSVS 2015 Update 2+ is supported.')
     try:
         boost_root = os.environ['BOOST_ROOT']
     except KeyError:
@@ -44,35 +56,21 @@ if sys.platform == 'win32':
     except KeyError:
         raise BuildError('BOOST_LIBRARYDIR environment variable is not defined!')
     library_dirs.append(boost_librarydir)
-    libraries=[
-        'libboost_regex-mt-s',
-        'libboost_system-mt-s',
-        'libboost_thread-mt-s',
-        'libboost_coroutine-mt-s',
-        'libboost_context-mt-s',
-        'libboost_filesystem-mt-s',
-        'libboost_date_time-mt-s',
-        'libboost_iostreams-mt-s',
-        'libboost_python-mt-s',
-        'libboost_zlib-mt-s',
-        ]
+    libraries.append('libboost_zlib')
+    libs = os.listdir(boost_librarydir)
+    for i, item in enumerate(libraries):
+        for lib in libs:
+            if item in lib:
+                libraries[i] = os.path.splitext(lib)[0]
+                break
+        else:
+           raise BuildError('Unable to find "{0}" library!'.format(item))
     define_macros.append(('BOOST_PYTHON_STATIC_LIB', None))
     extra_compile_args.append('/EHsk')
     extra_compile_args.append('/MT')
     extra_link_args.append('/SAFESEH:NO')
 else:
-    libraries=[
-        'boost_regex',
-        'boost_system',
-        'boost_thread',
-        'boost_coroutine',
-        'boost_context',
-        'boost_filesystem',
-        'boost_date_time',
-        'boost_iostreams',
-        'boost_python',
-        'z',
-        ]
+    libraries.append('z')
     extra_compile_args.append('-std=c++11')
 
 setup(name='wsgi_boost',
