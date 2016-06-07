9a3dfdbe9a1eef9199b6b71fba944570be48255e
Roman Miroshnychenko <romanvm@yandex.ua>
2016-06-07 15:01:08 +0300

    travis build

--- travis.py
+++ travis.py
@@ -3,9 +3,9 @@
 import os
 import sys
 
-config = ['using gcc : 5 : g++-5 ;']
+config = ['using gcc : 5 : g++-5 ;\n']
 if len(sys.argv) > 1:
-    config.append('using python : {0} ;'.format(sys.argv[1]))
+    config.append('using python : {0} ;\n'.format(sys.argv[1]))
 
 with open(os.path.expanduser('~/user-config.jam'), 'w') as fo:
     fo.writelines(config)
