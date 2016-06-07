5c2cca37a20bd8e957116c3d352edc896290ab62
Roman Miroshnychenko <romanvm@yandex.ru>
2016-06-07 22:19:55 +0300

    travis build

--- travis.py
+++ travis.py
@@ -7,7 +7,7 @@ import re
 config = ['using gcc : 5 : g++-5 ;\n']
 if len(sys.argv) > 1:
     py_version = re.search(r'^(\d+\.\d+)', sys.argv[1]).group(1)
-    config.append('using python : {0} ;\n'.format(py_version))
+    config.append('using python : {0} : /usr/bin/python{0} : /usr/include/python{0} : /usr/lib/x86_64-linux-gnu ;\n'.format(py_version))
 
 with open(os.path.expanduser('~/user-config.jam'), 'w') as fo:
     fo.writelines(config)
