7778d0d56656d216b5c1099aa03d61d21ad22b43
Roman Miroshnychenko <romanvm@yandex.ua>
2016-06-07 16:21:41 +0300

    Fixes obtaining Python version from Travis CI

--- travis.py
+++ travis.py
@@ -2,10 +2,12 @@
 
 import os
 import sys
+import re
 
 config = ['using gcc : 5 : g++-5 ;\n']
 if len(sys.argv) > 1:
-    config.append('using python : {0} ;\n'.format(sys.argv[1]))
+    py_version = re.search(r'^(\d+\.\d+)', sys.argv[1]).group(1)
+    config.append('using python : {0} ;\n'.format(py_version))
 
 with open(os.path.expanduser('~/user-config.jam'), 'w') as fo:
     fo.writelines(config)
