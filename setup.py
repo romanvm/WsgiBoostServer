34824cf9584e27f7fbe50f740f6cdc540899a93b
Roman Miroshnychenko <romanvm@yandex.ru>
2016-06-09 23:19:31 +0300

    Refactors setup.py: changes patching msvc compiler version, adds nose tests

--- setup.py
+++ setup.py
@@ -4,30 +4,34 @@ from __future__ import print_function
 import os
 import sys
 import re
+import distutils.msvc9compiler
 from setuptools import setup
 from setuptools.extension import Extension
 
-extension = 'wsgi_boost'
+NAME = 'wsgi_boost'
 
 SUPPORTED_MSVC_COMPILERS = [14.0]
 
 cwd = os.path.dirname(os.path.abspath(__file__))
-src = os.path.join(cwd, extension)
+src = os.path.join(cwd, NAME)
 
 
 class BuildError(Exception):
     pass
 
 
-def get_msvc_compiler():
+def patch_msvc_compiler():
     """
-    Returns environment path for a supported MS Visual C++ compiler
+    Monkey-patch distutils to use MS Visual C++ 2015 compiler
     """
     for vc_version in SUPPORTED_MSVC_COMPILERS:
-        compiler = 'VS{0}COMNTOOLS'.format(int(vc_version * 10))
-        if compiler in os.environ:
-            return os.environ[compiler]
-    raise BuildError('Compatible MS Visual C++ compiler not found! Only v. {0} are supported.'.format(str(SUPPORTED_MSVC_COMPILERS)))
+        vcvarsall = distutils.msvc9compiler.find_vcvarsall(vc_version)
+        if vcvarsall is not None:
+            distutils.msvc9compiler.get_build_version = lambda: vc_version
+            distutils.msvc9compiler.find_vcvarsall = lambda version: vcvarsall
+            break
+    else:
+        raise BuildError('Compatible MS Visual C++ compiler not found! Only v. {0} are supported.'.format(str(SUPPORTED_MSVC_COMPILERS)))
 
 
 def get_version():
@@ -46,15 +50,12 @@ include_dirs = [src]
 libraries = []
 library_dirs=[]
 
-define_macros = [('BOOST_PYTHON_STATIC_LIB', None)]
+define_macros = []
 extra_compile_args = []
 extra_link_args = []
 
 if sys.platform == 'win32':
-    from distutils.msvc9compiler import get_build_version
-    # Monkey-patching the default distutils compliler to a supported MS Visual C++ version
-    vs_comntools = 'VS{0}COMNTOOLS'.format(int(get_build_version() * 10))
-    os.environ[vs_comntools] = get_msvc_compiler()
+    patch_msvc_compiler()
 
     try:
         boost_root = os.environ['BOOST_ROOT']
@@ -67,19 +68,7 @@ if sys.platform == 'win32':
         raise BuildError('BOOST_LIBRARYDIR environment variable is not defined!')
     library_dirs.append(boost_librarydir)
 
-    boost_python = 'boost_python{0}.*?-mt-s[\.-]'
-    if sys.version_info.major >= 3:
-        boost_python = boost_python.format(sys.version_info.major)
-    else:
-        boost_python = boost_python.format('')
-    boost_py_re = re.compile(boost_python, re.IGNORECASE)
-    for lib in os.listdir(boost_librarydir):
-        if re.search(boost_py_re, lib) is not None:
-            libraries.append(os.path.splitext(lib)[0])
-            break
-    else:
-        raise BuildError('Unable to find boost_python library!')
-
+    define_macros.append(('BOOST_PYTHON_STATIC_LIB', None))
     extra_compile_args.append('/EHsk')
     extra_compile_args.append('/MT')
     extra_link_args.append('/SAFESEH:NO')
@@ -140,9 +129,11 @@ setup(
         ],
     platforms=['win32', 'posix'],
     zip_safe=False,
+    test_suite = 'nose.collector',
+    tests_require=['nose'],
     ext_modules=[
         Extension(
-            name=extension,
+            name=NAME,
             sources=sources,
             library_dirs=library_dirs,
             libraries=libraries,
