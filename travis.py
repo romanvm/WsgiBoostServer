f6e75ba99604bc2fb91e762974ce3bd1e5e04ada
Roman Miroshnychenko <romanvm@yandex.ru>
2016-06-10 23:10:49 +0300

    Minor change to Travis config

--- travis.py
+++ travis.py
@@ -4,13 +4,12 @@ Writes user-config.jam for Boost.Build
 """
 
 import os
-import sys
 import re
 
-config = ['using gcc : 5 : g++-5 ;\n']
-if len(sys.argv) > 1:
-    py_version = re.search(r'^(\d+\.\d+)', sys.argv[1]).group(1)
-    config.append('using python : {0} : /usr/bin/python{0} : /usr/include/python{0} : /usr/lib/x86_64-linux-gnu ;\n'.format(py_version))
-
+py_version = re.search(r'^(\d+\.\d+)', os.environ['TRAVIS_PYTHON_VERSION']).group(1)
+config = [
+    'using gcc : 5 : g++-5 ;\n',
+    'using python : {0} : /usr/bin/python{0} : /usr/include/python{0} : /usr/lib/x86_64-linux-gnu ;\n'.format(py_version),
+    ]
 with open(os.path.expanduser('~/user-config.jam'), 'w') as fo:
     fo.writelines(config)
