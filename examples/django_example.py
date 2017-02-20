1ff92ec171a71a6d10d04b70e3cc5489759c4c96
Roman Miroshnychenko <romanvm@yandex.ua>
2017-02-20 15:56:30 +0200

    Add Django example

--- examples/django_example.py
+++ examples/django_example.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python
 """
-This example demonstrates serving a Flask application
+This example demonstrates serving a Django application
 that returns a rendered web-page with static content (Bootstrap CSS and a picture)
 """
 
 import os
 import sys
-from flask import Flask, render_template
-import wsgi_boost
+from wsgi_boost import WsgiBoostHttp
 
 cwd = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__)
+sys.path.insert(0, os.path.join(cwd, 'django_example'))
 
-
-# Simple Flask application
-@app.route('/')
-@app.route('/<name>')
-def hello_world(name='Roman'):
-    return render_template('template.html', name=name)
-
+from django_example.wsgi import application
 
 if __name__ == '__main__':
     # Create a server on the default port 8000 with 4 threads
-    httpd = wsgi_boost.WsgiBoostHttp(threads=4)
+    httpd = WsgiBoostHttp(threads=4)
     # Serve static files by "static/*" path from "static" subfolder.
     # Also see template.html.
     httpd.add_static_route('^/static', os.path.join(cwd, 'static'))
-    # Set Flask WSGI application to be served
-    httpd.set_app(app)
+    # Set Django WSGI application to be served
+    httpd.set_app(application)
     httpd.start()
---
+++ examples/django_example/django_example/__init__.py
@@ -0,0 +1,1 @@
+"""A basic Django project to demonstrate WgsiBoostServer usage"""
