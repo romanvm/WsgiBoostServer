f29a22179c8b3aaaa7dbf1cdbe5c86e351edce6e
Roman Miroshnychenko <romanvm@yandex.ua>
2017-04-04 13:49:56 +0300

    Converted request.h to template

--- wsgi_boost/request.h
+++ wsgi_boost/request.h
@@ -10,6 +10,7 @@ License: MIT, see License.txt
 
 #include <boost/asio.hpp>
 #include <boost/functional/hash.hpp>
+#include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/regex.hpp>
 
@@ -50,10 +51,13 @@ namespace wsgi_boost
 		}
 	};
 
-
+	template <class conn_t>
 	// HTTP request parameters
 	class Request
 	{
+	private:
+		conn_t& m_connection;
+
 	public:
 		std::string method;
 		std::string path;
@@ -65,30 +69,91 @@ namespace wsgi_boost
 		Request(const Request&) = delete;
 		Request& operator=(const Request&) = delete;
 
-		explicit Request(Connection& connection) : m_connection{ connection } {}
+		explicit Request(conn_t& connection) : m_connection{ connection } {}
 
 		// Parse HTTP request headers
-		parse_result parse_header();
+		parse_result Request::parse_header()
+		{
+			std::string header;
+			sys::error_code ec = m_connection.read_header(header);
+			if (ec)
+				return CONN_ERROR;
+			std::istringstream iss{ header };
+			std::string line;
+			std::getline(iss, line);
+			line.pop_back();
+			std::vector<string> parts;
+			boost::algorithm::split(parts, line,
+				boost::algorithm::is_space(),
+				boost::algorithm::token_compress_on);
+			if (parts.size() != 3)
+				return BAD_REQUEST;
+			method = parts[0];
+			path = parts[1];
+			http_version = parts[2];
+			while (true)
+			{
+				std::getline(iss, line);
+				if (!iss.good())
+					break;
+				line.pop_back();
+				size_t pos = line.find(':');
+				if (pos != std::string::npos)
+				{
+					std::string name = boost::algorithm::trim_copy(line.substr(0, pos));
+					std::string value = boost::algorithm::trim_copy(line.substr(pos + 1));
+					if (headers.find(name) == headers.end())
+						headers.emplace(name, value);
+					else
+						headers[name] += ", " + value;
+				}
+			}
+			if (method == "POST" || method == "PUT" || method == "PATCH")
+			{
+				try
+				{
+					long long cl = stoll(get_header("Content-Length"));
+					m_connection.post_content_length(cl);
+				}
+				catch (const std::logic_error&)
+				{
+					return LENGTH_REQUIRED;
+				}
+			}
+			else
+			{
+				m_connection.post_content_length(-1);
+			}
+			return PARSE_OK;
+		}
 
 		// Check if a header contains a specific value
-		bool check_header(const std::string& header, const std::string& value) const;
+		bool check_header(const std::string& header, const std::string& value) const
+		{
+			auto it = headers.find(header);
+			return (it != headers.end()) && boost::algorithm::icontains(it->second, value);
+		}
 
 		// Get header value or "" if the header is missing
-		std::string get_header(const std::string& header) const;
+		std::string get_header(const std::string& header) const
+		{
+			auto it = headers.find(header);
+			return it != headers.end() ? it->second : string();
+		}
 
 		// Check if the connection is persistent (keep-alive)
-		bool keep_alive() const;
+		bool keep_alive() const
+		{
+			return check_header("Connection", "keep-alive") || (http_version == "HTTP/1.1" && !check_header("Connection", "close"));
+		}
 
 		// Get Connection object for this request
-		Connection& connection() const;
+		conn_t& connection() const { return m_connection; }
 
 		// Get remote endpoint address
-		std::string remote_address() const;
+		std::string remote_address() const { return m_connection.socket()->lowest_layer().remote_endpoint().address().to_string(); }
 
 		// Get remote endpoint port
-		unsigned short remote_port() const;
-
-	private:
-		Connection& m_connection;
+		unsigned short remote_port() const { m_connection.socket()->lowest_layer().remote_endpoint().port() }
 	};
 }
